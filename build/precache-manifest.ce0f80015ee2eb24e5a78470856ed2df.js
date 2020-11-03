7b65ccd6a75e081cb8d39ffd1a60903f8e277acf
Leoric Lin <firecrack123@gmail.com>
2020-11-04 02:30:17 +0800

    2nd commit

---
+++ build/precache-manifest.ce0f80015ee2eb24e5a78470856ed2df.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b440d318eab8ad9b5bc97ba3a210d72b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "588f4abbc6e6bf813170",
+    "url": "./static/css/3.d9b811cd.chunk.css"
+  },
+  {
+    "revision": "e376327c05523e579c86",
+    "url": "./static/css/4.153276d2.chunk.css"
+  },
+  {
+    "revision": "3f0a8cb8848d10be25cb",
+    "url": "./static/css/5.5f853bfa.chunk.css"
+  },
+  {
+    "revision": "8a4d17d2044527cb4008",
+    "url": "./static/css/6.3c5285f5.chunk.css"
+  },
+  {
+    "revision": "0cd5c2ca8c53761c9922",
+    "url": "./static/css/main.9b011ed1.chunk.css"
+  },
+  {
+    "revision": "a194ffc7ae9433a95076",
+    "url": "./static/js/2.dc724a8a.chunk.js"
+  },
+  {
+    "revision": "7bbfdfaa620dfc65b72502e4aad0eb44",
+    "url": "./static/js/2.dc724a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588f4abbc6e6bf813170",
+    "url": "./static/js/3.1cdf2f09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/3.1cdf2f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e376327c05523e579c86",
+    "url": "./static/js/4.073a6eee.chunk.js"
+  },
+  {
+    "revision": "3f0a8cb8848d10be25cb",
+    "url": "./static/js/5.8095fc63.chunk.js"
+  },
+  {
+    "revision": "8a4d17d2044527cb4008",
+    "url": "./static/js/6.40b04ecd.chunk.js"
+  },
+  {
+    "revision": "c011c7aea25b9f53c4d6",
+    "url": "./static/js/7.39aa3a3e.chunk.js"
+  },
+  {
+    "revision": "0cd5c2ca8c53761c9922",
+    "url": "./static/js/main.ab59a48d.chunk.js"
+  },
+  {
+    "revision": "ee862a2367b2955dba26",
+    "url": "./static/js/runtime-main.f193cb4b.js"
+  },
+  {
+    "revision": "8791667d70fd03da986bc19d9eb178e0",
+    "url": "./static/media/bg3.8791667d.jpg"
+  }
+]);
