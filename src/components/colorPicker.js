44a03892a5edf313e8549b260079d0b64d2508a6
Leo <goldcarrotminer@gmail.com>
2021-10-09 23:45:08 +0800

    Reworked Color Picker to include color generation

--- src/components/colorPicker.js
+++ src/components/colorPicker.js
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "../styles/color.css";
+
 export default function Color() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("white");
+
+  function genColorCode() {
+    let code;
+    do {
+      code = Math.floor(Math.random() * 16777216).toString(16);
+    } while (code.length !== 6);
+
+    return "#" + code.toUpperCase();
+  }
+
+  function copyCode() {
+    navigator.clipboard.writeText(color);
+  }
+
+  const [contOffset, setContOffset] = useState(0);
   useEffect(() => {
-    document.body.style.backgroundColor = color;
-  }, [color]);
-  useEffect(() => {
-    return () => {
-      document.body.style.background = "white";
-    };
+    setContOffset(document.querySelector(".color-container")?.offsetTop);
   }, []);
+
   return (
-    <div className="color-container">
+    <div
+      className="color-container"
+      style={{
+        background: color,
+        height: `calc(100vh - ${contOffset}px)`,
+      }}
+    >
       <input
         placeholder="Enter color code"
         value={color}
         onChange={(e) => setColor(e.target.value.toUpperCase())}
         onFocus={(e) => e.target.select()}
-        style={{color:color}}
+        style={{ color: color }}
+        onClick={copyCode}
       />
+
+      <button style={{ color }} onClick={() => setColor(genColorCode())}>
+        Generate Color!
+      </button>
     </div>
   );
 }
