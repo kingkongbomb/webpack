7512b84cb2c79e99af45c604796c852d86dfc5a2
Leo <goldcarrotminer@gmail.com>
2022-07-22 23:02:47 +0800

    Refactored some code in navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,45 +1,54 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const { Link } = Nav;
+const { Toggle, Collapse } = Navbar;
+const { Item: NavItem } = NavDropdown;
 
 export default function NavBS(props) {
   return (
-    <Navbar collapseOnSelect expand="md" fixed={props.fixed} bg={props.trans} variant="dark" onToggle={toggle=> toggle? console.log("OPEN") :console.log("CLOSE")}>
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      fixed={props.fixed}
+      bg={props.trans}
+      variant="dark"
+    >
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Toggle aria-controls="basic-navbar-nav" />
+        <Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link as={NavLink} eventKey="1" to="/" exact>
-              Home 
-            </Nav.Link>
-            <Nav.Link as={NavLink} eventKey="2" to="/TodoApp" >
+            <Link as={NavLink} eventKey="1" to="/" exact>
+              Home
+            </Link>
+            <Link as={NavLink} eventKey="2" to="/TodoApp">
               Todo
-            </Nav.Link>
-            <Nav.Link as={NavLink} eventKey="3" to="/ColorApp">
+            </Link>
+            <Link as={NavLink} eventKey="3" to="/ColorApp">
               Color Picker
-            </Nav.Link>
-            <Nav.Link as={NavLink} eventKey="4" to="/Covid">
+            </Link>
+            <Link as={NavLink} eventKey="4" to="/Covid">
               SG Covid Tracker
-            </Nav.Link>
+            </Link>
             <NavDropdown title="JS Frameworks" id="basic-nav-dropdown">
-              <NavDropdown.Item
+              <NavItem
                 target="_blank"
                 href="https://comcalc.neocities.org/vue/#/"
               >
                 Vue
-              </NavDropdown.Item>
-              <NavDropdown.Item
+              </NavItem>
+              <NavItem
                 target="_blank"
                 href="http://aspiring-argument.surge.sh/"
               >
                 Gatsby
-              </NavDropdown.Item>
+              </NavItem>
             </NavDropdown>
           </Nav>
-        </Navbar.Collapse>
+        </Collapse>
       </Container>
     </Navbar>
   );
 }
-
