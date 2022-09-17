90b6c7da566b5f341902bdec694e04fe947be67f
Leo <goldcarrotminer@gmail.com>
2022-09-17 19:00:21 +0800

    Word hack Fixed, now functional

--- src/pages/wordHack.js
+++ src/pages/wordHack.js
@@ -6,30 +6,24 @@ export default function WordHack() {
   const [wordMap, setWordMap] = useState({});
   const [renderWords, setRenderWords] = useState([]);
   const [ans, setAns] = useState([]);
+  const [charMap, setCharMap] = useState({});
+  const [helpWords, setHelpWords] = useState("");
 
-  const fetchWordList = async () => {
+  const initWordList = async () => {
     const res = await fetch("./resources/enWords10k.txt");
     const data = await res.text();
-    setAllWords(data.split("\n"));
+    const dataArr = data.split("\r\n");
+    setAllWords(dataArr);
+    grpWordsByLength(dataArr);
   };
 
-  useEffect(async () => {
-    const res = await fetch("./resources/enWords10k.txt");
-    const data = await res.text();
-    setAllWords(data.split("\n"));
-    grpWordsByLength(data.split("\n"));
+  useEffect(() => {
+    initWordList();
   }, []);
 
   const grpWordsByLength = (words) => {
     const map = {};
-    // allWords.forEach((word) => {
-    //   const length = word.length;
-    //   if (map[length] === undefined) {
-    //     map[length] = [word];
-    //   } else {
-    //     map[length].push(word);
-    //   }
-    // });
+
     for (let x = 0; x < words.length; x++) {
       const word = words[x];
       const length = word.length;
@@ -44,12 +38,10 @@ export default function WordHack() {
   };
 
   const getWords = (e) => {
-    const length = e.target.value;
+    const length = parseInt(e.target.value) || 0;
     setRenderWords(wordMap[length] || []);
     setAns(Array(parseInt(length || 0)).fill(""));
   };
-
-  const [charMap, setCharMap] = useState({});
 
   const inputChar = (val, idx) => {
     let tempCharMap = { ...charMap };
@@ -60,30 +52,65 @@ export default function WordHack() {
     setAns(tempAns);
   };
 
+  // Pass in an array of words
+  // Returns an array of words that are valid
+  // Check validity - there should not be a word NOT found in helpWords
+  function filterHelper(words, helpWords) {
+    const result = words.filter((word) => {
+      let match = true;
+      word.split("").forEach((char) => {
+        if (helpWords.includes(char) === false) {
+          match = false;
+        }
+      });
+      return match;
+    });
+
+    return result;
+  }
+
+  const RenderList = () => {
+    return filterHelper(renderWords, helpWords)
+      .filter((word) => {
+        let toShow = true;
+
+        Object.keys(charMap).forEach((key) => {
+          if (charMap[key] !== word[key] && charMap[key] !== "") {
+            toShow = false;
+          }
+        });
+        return toShow;
+      })
+      .map((word) => <div key={word}>{word}</div>);
+  };
+
   return (
     <Container>
-      <Input placeholder="Word Length" onChange={getWords} />
+      <Input
+        placeholder="Possible Words"
+        value={helpWords}
+        onChange={(e) => {
+          setCharMap({}); //Clear charMap
+          setHelpWords(e.target.value.toLowerCase());
+        }}
+      />
+      <Input
+        placeholder="Word Length"
+        onChange={(e) => {
+          setCharMap({}); //Clear charMap
+          getWords(e);
+        }}
+      />
       <div style={{ display: "flex", gap: 20 }}>
         {ans.map((char, idx) => (
           <Input
+            key={idx}
             value={char}
             onChange={(e) => inputChar(e.target.value, idx)}
           />
         ))}
       </div>
-      {renderWords
-        .filter((word) => {
-          let toShow = true;
-          Object.keys(charMap).forEach((key) => {;
-            if (charMap[key] !== word[key]) {
-              toShow = false;
-            }
-          });
-          return toShow;
-        })
-        .map((word) => (
-          <div key={word}>{word}</div>
-        ))}
+      <RenderList />
     </Container>
   );
 }
