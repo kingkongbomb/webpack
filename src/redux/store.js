496d596ef7abd22a0a038bc64013b8afe5beb8a7
Leo <goldcarrotminer@gmail.com>
2022-12-31 14:22:29 +0800

    feat: use store.subscribe to persist state changes to localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { loadState, saveState } from "../utils/localstorage";
 import todos from "./slices/todos";
 
 const store = configureStore({
   reducer: {
     todo: todos,
   },
+  preloadedState: loadState(),
 });
+
+store.subscribe(() => saveState(store.getState()));
 
 export { store };
